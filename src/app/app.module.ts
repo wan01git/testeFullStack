fd6d5fc2ad191cccde3e72e06cede2ce19a8e1d6
Wandeson <wanderson.gustavo@structor.com.br>
2024-02-02 08:33:03 -0300

    feat:layout3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Layout2Component } from './layout2/layout2.component';
+import { Layout3Component } from './layout3/layout3.component';
 
 @NgModule({
-  declarations: [AppComponent, Layout2Component],
+  declarations: [AppComponent, Layout2Component, Layout3Component],
   imports: [BrowserModule, MatIconModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/layout3/layout3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout3',
+  templateUrl: './layout3.component.html',
+  styleUrl: './layout3.component.css'
+})
+export class Layout3Component {
+
+}
