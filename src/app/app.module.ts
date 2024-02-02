ef97ebd6f83426c5a06cd34fdb6e79e79188f850
Wandeson <wanderson.gustavo@structor.com.br>
2024-02-01 22:57:43 -0300

    feat: layout2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Layout2Component } from './layout2/layout2.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, Layout2Component],
   imports: [BrowserModule, MatIconModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/layout2/layout2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout2',
+  templateUrl: './layout2.component.html',
+  styleUrl: './layout2.component.css'
+})
+export class Layout2Component {
+
+}
